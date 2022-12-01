ab9122a31053d7b43455e8450ee787c4cd11acc4
Nancy <119377166+fupingnie@users.noreply.github.com>
2022-12-01 16:59:46 +0800

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -33,7 +33,9 @@ try:
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   streamlit.dataframe(fruityvice_normalized)
-
+  
+ except URLError as e:
+  urllib.error()
 
 #streamlit.write('The user entered ', fruit_choice)
 # write your own comment -what does the next line do? 
