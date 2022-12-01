7d2a4ebbe149bd4d7021a561693aff98b00a2c86
Nancy <119377166+fupingnie@users.noreply.github.com>
2022-12-01 17:20:11 +0800

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLError
+import urllib.error
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -26,13 +26,13 @@ streamlit.dataframe(fruit_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
- fruit_choice = streamlit.text_input('What fruit would you like information?')
- if not fruit_choice:
-  streamlit.error("please select a fruit to get information.")
- else:
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruityvice_normalized)
+  fruit_choice = streamlit.text_input('What fruit would you like information?')
+  if not fruit_choice:
+   streamlit.error("please select a fruit to get information.")
+  else:
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   streamlit.dataframe(fruityvice_normalized)
   
  except URLError as e:
   streamlit.error()
